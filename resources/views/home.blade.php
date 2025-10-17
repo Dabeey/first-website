e7afb421888607e6ed7a8015fd7f9b587374dc21
Dabeey <abbasdaughter18@gmail.com>
2025-10-17 13:49:19 +0100

    Update home view to include a form with validation and modify routes for form submission, enhancing user input handling and feedback.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,15 @@
 </head>
 <body>
     <h1>Home Page</h1>
-    <form action="/formsubmitted">
-        <!-- <label for=""></label> -->
-         <input type="text" id="fullname" placeholder="Type Your full  name!">
+    <form action="{{ route('formsubmitted') }}" method="POST"> 
+        @csrf
+        <label for="email">Full name</label>
+        <input type="text" name="fullname" placeholder="Type Your full  name!" required>
+        <br><br>
+        <label for="email">E-Mail</label>
+        <input type="text" name="email" placeholder="Type your email!" required>
+        <br><br>
+        <button type="submit">Submit</button>
     </form>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 // Get route example
 Route::get('/', function () {
@@ -8,8 +9,18 @@ Route::get('/', function () {
 });
 
 //  POST route example
-Route::post('/formsubmitted', function() {
-    return 'Form submitted';
-});
+Route::post('/formsubmitted', function(Request $request) {
+    
+    $request->validate([
+        'fullname' => 'required | min:3 | max:30',
+        'email' => 'required | min:3 | max:30 | email',
+    ]);
+    
+    $fullname = $request->input('fullname');
+    $email = $request->input('email');
+
+    return "Welcome $fullname!. It is nice to have you. Gift card is sent to $email!";
+
+})->name('formsubmitted');
 
 
