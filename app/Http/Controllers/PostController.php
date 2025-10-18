92a8c19598a2e7617113705103fde55d014e2114
Dabeey <abbasdaughter18@gmail.com>
2025-10-18 12:19:18 +0100

    Add PostController methods for listing and creating posts, and set up resource routes for posts.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
     }
 
     /**
@@ -20,7 +21,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>All Posts</title>
+</head>
+<body>
+    <h1>All Posts</h1>
+    
+    @if($posts->count())
+        <ul>
+        @foreach($posts as $post)
+            <li>
+                <strong>{{ $post->title }}</strong>
+                <p>{{ $post->body }}</p>
+            </li>
+        @endforeach
+        </ul>
+    @else
+        <p>No posts found!</p>
+    @endif
+
+    <a href="{{ route('create')}}">Create New Post</a>
+</body>
+</html>
